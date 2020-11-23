f0b7e27ca4f72b53779760562bf9fbd76f578257
Te4-Dennis-Christensen <dennis.christensen@elev.ga.ntig.se>
2020-11-23 14:17:55 +0100

    start of form

---
+++ log-o-matic/src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <input type="text" name="email" v-model="email">
+    <input type="password" name="password" v-model="password">
+    <button @onClick="login()">Login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+name: "Home",
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    login(){
+      console.log(123)
+    }
+  }
+});
+</script>
