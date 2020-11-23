ff0f666f8357a8b686df8496e6cd05654ac1cfe4
Jonathan Ronsten <jonathan.ronsten@elev.ga.ntig.se>
2020-11-23 14:46:08 +0100

    asdasd

---
+++ log-o-matic/src/components/showLogs.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "logsTemplate",
+  data() {
+    return {
+      headers: [
+        {
+          text: "Student",
+          align: "start",
+          sortable: false,
+          value: "name"
+        },
+        { text: "Monday", value: "monday" },
+        { text: "Tuesday", value: "tuesday" },
+        { text: "Wednesday", value: "wednesday" },
+        { text: "Thursday", value: "thursday" },
+        { text: "Friday", value: "friday" }
+      ],
+      data: [
+        {
+          name: "Jonathan Ronsten",
+          monday: "good",
+          tuesday: "bad",
+          wednesday: "good",
+          thursday: "bad",
+          friday: "good"
+        },
+        {
+          name: "Dennis",
+          monday: "good",
+          tuesday: "bad",
+          wednesday: "good",
+          thursday: "bad",
+          friday: "good"
+        },
+        {
+          name: "Filip",
+          monday: "good",
+          tuesday: "bad",
+          wednesday: "good",
+          thursday: "bad",
+          friday: "good"
+        },
+        {
+          name: "Gomezzzz",
+          monday: "good",
+          tuesday: "bad",
+          wednesday: "good",
+          thursday: "bad",
+          friday: "good"
+        }
+      ]
+    };
+  }
+});
+</script>
