ce8db7c233b3b10a405fee8be0e1dcb19d7ba646
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2020-11-24 14:41:55 +0100

    Finishd all students

--- log-o-matic/src/store/index.ts
+++ log-o-matic/src/store/index.ts
@@ -7,21 +7,33 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     teachers: {
-      1: {email: 'daniel.berg@ga.ntig.se', name: 'Daniel Berg', password: ""},
-      2: {email: 'daniel.berg@ga.ntig.se', name: 'Daniel Berg', password: ""},
-      3: {email: 'daniel.berg@ga.ntig.se', name: 'Daniel Berg', password: ""},
-      4: {email: 'daniel.berg@ga.ntig.se', name: 'Daniel Berg', password: ""},
+      1: { email: "daniel.berg@ga.ntig.se", name: "Daniel Berg", password: "" },
+      2: { email: "daniel.berg@ga.ntig.se", name: "Del Berg", password: "" },
+      3: { email: "daniel.berg@ga.ntig.se", name: "Kaktus Berg", password: "" },
+      4: { email: "daniel.berg@ga.ntig.se", name: "Ananas Berg", password: "" }
     },
     user: {
       id: 1
+    },
+    
+    students: {
+      1: { name: "Kalle", teacherId: 3, password: "", email: "abc@gmail.com" },
+      2: { name: "Kalle", teacherId: 1, password: "", email: "abc@gmail.com" },
+      3: { name: "Kalle", teacherId: 2, password: "", email: "abc@gmail.com" },
+      4: { name: "Kalle", teacherId: 2, password: "", email: "abc@gmail.com" },
+      5: { name: "Kalle", teacherId: 4, password: "", email: "abc@gmail.com" }
     }
   },
   mutations: {},
   actions: {},
   modules: {},
   getters: {
-    teachers(state){
-      return state.teachers
+    teachers(state) {
+      return state.teachers;
+    },
+
+    students(state) {
+      return state.students;
     }
   }
 });
