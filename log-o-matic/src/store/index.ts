8c20bde136cb873187cd27807477cf7ec512a915
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2020-11-24 12:41:27 +0100

    All teachers

--- log-o-matic/src/store/index.ts
+++ log-o-matic/src/store/index.ts
@@ -1,11 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { Module } from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    teachers: {
+      1: {email: 'daniel.berg@ga.ntig.se', name: 'Daniel Berg', password: ""},
+      2: {email: 'daniel.berg@ga.ntig.se', name: 'Daniel Berg', password: ""},
+      3: {email: 'daniel.berg@ga.ntig.se', name: 'Daniel Berg', password: ""},
+      4: {email: 'daniel.berg@ga.ntig.se', name: 'Daniel Berg', password: ""},
+    },
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    teachers(state){
+      return state.teachers
+    }
+  }
 });
