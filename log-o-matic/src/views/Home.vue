6bb9a8f1e64e89795ed3cea3c4e6a9add2e2f114
Te4-Dennis-Christensen <dennis.christensen@elev.ga.ntig.se>
2020-11-24 13:29:16 +0100

    fix

--- log-o-matic/src/views/Home.vue
+++ log-o-matic/src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <h1 class="mt-8">Welcome to Log-O-matic</h1>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 });
 </script>
